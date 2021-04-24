d2c1e9458772cb8105b1352fb6df02492589a402
Haris.hussain <harishussain33@gmail.com>
2021-04-24 12:34:48 +0500

    Googles Maps Added

--- app/src/main/java/com/haris/commutesystem/MainNavParent.java
+++ app/src/main/java/com/haris/commutesystem/MainNavParent.java
@@ -2,13 +2,29 @@ package com.haris.commutesystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainNavParent extends AppCompatActivity {
+
+    private Button btnView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_nav_parent);
+
+        btnView = findViewById(R.id.btnViewDriver);
+
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainNavParent.this, DriverLocationActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
