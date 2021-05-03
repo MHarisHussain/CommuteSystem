09bca52209d6f8a0410d7b8929b60858788ba6c1
MHarisHussain <harishussain33@gmail.com>
2021-05-03 14:32:35 +0500

    adding firebase auth

--- app/src/main/java/com/haris/commutesystem/DriverActivity.java
+++ app/src/main/java/com/haris/commutesystem/DriverActivity.java
@@ -1,15 +1,25 @@
 package com.haris.commutesystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.developer.mtextfield.ExtendedEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class DriverActivity extends AppCompatActivity {
 
     private Button btnLogin,btnBack;
+    private ExtendedEditText mEmail, mPassword;
+
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener mAuthListner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +28,28 @@ public class DriverActivity extends AppCompatActivity {
 
         btnLogin = findViewById(R.id.btnLoginDriver);
         btnBack = findViewById(R.id.btnBackD);
+        auth = FirebaseAuth.getInstance();
+
+        mAuthListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+
+                }
+
+            }
+        };
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String email = mEmail.getText().toString();
+                String pass = mPassword.getText().toString();
+                if(!email.equals("") && !pass.equals("")){
+                    auth.signInWithEmailAndPassword(email, pass);
+                }
                 Intent intent = new Intent(DriverActivity.this, MainNavDriver.class);
                 startActivity(intent);
                 finish();
@@ -36,5 +64,19 @@ public class DriverActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListner != null){
+            auth.removeAuthStateListener(mAuthListner);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(mAuthListner);
     }
 }
