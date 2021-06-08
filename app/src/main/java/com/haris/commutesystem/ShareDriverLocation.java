df164aa265fa4deb4a472061364bf22fd42b94f8
MHarisHussain <harishussain33@gmail.com>
2021-06-08 09:38:57 +0500

    List View Added with Checkbox

--- app/src/main/java/com/haris/commutesystem/ShareDriverLocation.java
+++ app/src/main/java/com/haris/commutesystem/ShareDriverLocation.java
@@ -1,95 +1,84 @@
 package com.haris.commutesystem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckedTextView;
+import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.Marker;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-public class ShareDriverLocation extends AppCompatActivity implements LocationListener {
+public class ShareDriverLocation extends AppCompatActivity {
 
-    private GoogleMap mMap;
-    private DatabaseReference reference;
-    private LocationManager manager;
-    private final int MIN_TIME = 1000; // 1 sec
-    private final int MIN_DISTANCE = 1; // 1 meter
-    Marker myMarker;
+    private ListView listView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_driver_location);
-        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        reference = FirebaseDatabase.getInstance().getReference().child("User-101");
-        Toast.makeText(this, "Location Sharing", Toast.LENGTH_SHORT).show();
-        getLocationUpdates();
-    }
 
-    private void getLocationUpdates() {
-        if(manager != null){
-            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                    manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
-                } else if (manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-                    manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
-                } else {
-                    Toast.makeText(this, "No Provider Enable", Toast.LENGTH_SHORT).show();
-                }
-            }else{
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},101);
+        this.listView = (ListView)findViewById(R.id.listView);
+        this.button = (Button)findViewById(R.id.button);
+
+        // CHOICE_MODE_NONE: (Default)
+        // (listView.setItemChecked(..) doest not work with CHOICE_MODE_NONE).
+        // CHOICE_MODE_SINGLE:
+        // CHOICE_MODE_MULTIPLE:
+        // CHOICE_MODE_MULTIPLE_MODAL:
+        this.listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                CheckedTextView v = (CheckedTextView) view;
+                boolean currentCheck = v.isChecked();
+                UserAccount user = (UserAccount) listView.getItemAtPosition(position);
+                user.setActive(!currentCheck);
             }
-        }
-    }
+        });
+        //
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == 101){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                getLocationUpdates();
-            }else{
-                Toast.makeText(this, "Premission Required", Toast.LENGTH_SHORT).show();
+        this.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                printSelectedItems();
             }
+        });
+
+        this.initListViewData();
+    }
+
+    private void initListViewData()  {
+        UserAccount tom = new UserAccount("Huda","Student", false);
+        UserAccount jerry = new UserAccount("Fatima","Student", false);
+        UserAccount donald = new UserAccount("Hadi","Student", false);
+
+        UserAccount[] users = new UserAccount[]{tom,jerry, donald};
+
+        // android.R.layout.simple_list_item_checked:
+        // ListItem is very simple (Only one CheckedTextView).
+        ArrayAdapter<UserAccount> arrayAdapter
+                = new ArrayAdapter<UserAccount>(this, android.R.layout.simple_list_item_checked , users);
+
+        this.listView.setAdapter(arrayAdapter);
+
+        for(int i=0;i< users.length; i++ )  {
+            this.listView.setItemChecked(i,users[i].isActive());
         }
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        if(location != null){
-            saveLocation(location);
-        }else{
-            Toast.makeText(this, "No Location", Toast.LENGTH_SHORT).show();
-        }
+    public void printSelectedItems()  {
+        Intent intent = new Intent(ShareDriverLocation.this, DriverLoginActivity.class);
+        startActivity(intent);
+        Toast.makeText(this, "Trip Completed", Toast.LENGTH_LONG).show();
     }
 
-    private void saveLocation(Location location) {
-        reference.setValue(location);
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
 }
